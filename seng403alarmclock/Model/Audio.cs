e4e0c5bfdeb6df1c635a38b9013fd80fd74f02a8
Devon Gillis <Devon Gillis@DESKTOP-0BFQ1FS>
2017-02-27 21:49:53 -0700

    one more change audio files

--- seng403alarmclock/Model/Audio.cs
+++ seng403alarmclock/Model/Audio.cs
@@ -14,6 +14,7 @@ namespace seng403alarmclock.Model
         {
             player = new SoundPlayer();
             player.SoundLocation = @"AudioFiles\" + audioID + ".wav";
+
         }
 
         public void start()
