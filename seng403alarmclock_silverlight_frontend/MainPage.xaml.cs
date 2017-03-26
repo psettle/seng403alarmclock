36bba82ebb6098d493c622863faf007abcb9ab0c
psettle <patricksettle@hotmail.com>
2017-03-25 21:02:52 -0600

    Created a slide in out panel

--- seng403alarmclock_silverlight_frontend/MainPage.xaml.cs
+++ seng403alarmclock_silverlight_frontend/MainPage.xaml.cs
@@ -7,14 +7,31 @@ namespace seng403alarmclock_silverlight_frontend {
     /// This is the main page of the app
     /// </summary>
     public partial class MainPage : UserControl {
+
+        private static bool AddEditPanelIsIn = false;
+        
         /// <summary>
         /// Initializes the main page
         /// </summary>
         public MainPage() {
             InitializeComponent();
             GuiController.GetController().assignMainPage(this);
-
+            button.Click += Button_Click;
+            //AddEditSlide.AutoReverse = true;
         }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e) {
+            if(AddEditPanelIsIn) {
+                AddEditSlideOut.Begin();
+            } else {
+                AddEditSlideIn.Begin();
+            }
+
+            AddEditPanelIsIn = !AddEditPanelIsIn;
+            
+        }
+
+
 
         /// <summary>
         /// Sets the time display on the GUI
