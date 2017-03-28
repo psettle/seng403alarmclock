bc1b8e1bbda76cd234f57275dbb68c3742844fff
psettle <patricksettle@hotmail.com>
2017-03-28 00:30:41 -0600

    Documented audio

--- seng403alarmclock_silverlight_frontend/Audio/Audio.cs
+++ seng403alarmclock_silverlight_frontend/Audio/Audio.cs
@@ -8,23 +8,41 @@ using System.Windows;
 using System.Windows.Controls;
 
 namespace seng403alarmclock_silverlight_frontend.Audio {
+    /// <summary>
+    /// Audio driver for silverlight
+    /// </summary>
     public class Audio : AudioI {
-
+        /// <summary>
+        /// The player for audio code
+        /// </summary>
         private MediaElement me;
 
+        /// <summary>
+        /// Indicates if the audio should be playing
+        /// </summary>
         private bool playing = false;
 
-        public void buildForAudioFile(string filename) {  
+        /// <summary>
+        /// Sets up the audio object for playing filename
+        /// </summary>
+        public void buildForAudioFile(string filename) {
+            //stop what is currently running
             end();
 
+            //create the media player
             me = new MediaElement();
             me.AutoPlay = false;
+
+            //start opening a stream to the audio file
             WebClient client = new WebClient();
             client.OpenReadAsync(new Uri(Application.Current.Host.Source, "/" + filename));
             client.OpenReadCompleted += Client_OpenReadCompleted;
          
         }
 
+        /// <summary>
+        /// Connects the filestream with the media player after the connection is established
+        /// </summary>
         private void Client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e) {
             Stream s = e.Result;
             me.SetSource(s); 
@@ -33,6 +51,9 @@ namespace seng403alarmclock_silverlight_frontend.Audio {
             }
         }
 
+        /// <summary>
+        /// Stops the currently playing audio
+        /// </summary>
         public void end() {
             playing = false;
             if (me != null)
@@ -41,6 +62,12 @@ namespace seng403alarmclock_silverlight_frontend.Audio {
             }
         }
 
+        /// <summary>
+        /// Starts the audio
+        /// 
+        /// Note:
+        /// If the stream hasn't been attached, it will start automatically when the remote request completes
+        /// </summary>
         public void start() {
             playing = true;
             if (me != null)
